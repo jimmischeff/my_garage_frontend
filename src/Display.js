065b94c8c934bfd9a1a5bbb8d834b20468a250bb
jimmischeff <jimmischeff@gmail.com>
2020-12-06 14:34:11 -0500

    added state to Display.js

--- src/Display.js
+++ src/Display.js
@@ -4,7 +4,19 @@ const Display = (props) => {
     const {vehicles} = props
 
     const loaded = () => (
-        <div>Vehicle info goes here</div>
+        <div>
+            {vehicles.map((vehicle) => (
+                <div>
+                <article key={vehicle.id}>
+                    <div>
+                        <h2>{vehicle.year}</h2>
+                        <h1>{vehicle.model}</h1>
+                        <img src={vehicle.img}/>
+                    </div>
+                </article>
+                </div>
+            ))}
+        </div>
     )
 
     const loading = <h1>Loading...</h1>
