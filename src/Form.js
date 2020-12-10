3699572b801d53af44d01f56eac32028bb91b76d
jimmischeff <jimmischeff@gmail.com>
2020-12-10 17:38:33 -0500

    added styling to form page

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Button, Form, FormGroup, Label, Input, FormText, Row, Col } from 'reactstrap'
 
 const VehicleForm = (props) => {
     //Form state
@@ -15,41 +16,92 @@ const VehicleForm = (props) => {
         setFormData({...formData, [event.target.name]: event.target.value })
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <label for='addVehicle'>Add Vehicle</label>
-
-            <input 
-            type='text' 
-            name="color" 
-            value={formData.color} 
-            onChange={handleChange}
-            />
-            <input 
-            type='text' 
-            name="img" 
-            value={formData.img} 
-            onChange={handleChange}
-            />
-            <input 
-            type='text' 
-            name="make" 
-            value={formData.make} 
-            onChange={handleChange}
-            />
-            <input 
-            type='text' 
-            name="model" 
-            value={formData.model} 
-            onChange={handleChange}
-            />
-            <input 
-            type='number' 
-            name="year" 
-            value={formData.year} 
-            onChange={handleChange}
-            />
-            <input type="submit" value={props.label} />
-        </form>
+        <Form onSubmit={handleSubmit}>
+            <Label for='addVehicle'>Add Vehicle</Label>
+            <Row form>
+                <Col md={2}>
+                    <FormGroup>
+                    <Label>Year:</Label>
+                    <Input 
+                    type='number' 
+                    name="year" 
+                    value={formData.year} 
+                    onChange={handleChange}
+                    />
+                    </FormGroup>
+                </Col>
+                <Col md={5}>
+                    <FormGroup>
+                    <Label>Make:</Label>
+                    <Input 
+                    type='text' 
+                    name="make" 
+                    value={formData.make} 
+                    onChange={handleChange}
+                    />
+                    </FormGroup>
+                </Col>
+                <Col md={5}>
+                    <FormGroup>
+                    <Label>Model:</Label>
+                    <Input 
+                    type='text' 
+                    name="model" 
+                    value={formData.model} 
+                    onChange={handleChange}
+                    />
+                    </FormGroup>
+                </Col>
+            </Row>
+            <Row form>
+                <Col md={4}>
+            <FormGroup>
+                <Label>Color:</Label>
+                <Input 
+                type='text' 
+                name="color" 
+                value={formData.color} 
+                onChange={handleChange}
+                />
+            </FormGroup>
+            </Col>
+            <Col md={8}>
+                <Label>Image:</Label>
+                <Input 
+                type='text' 
+                name="img" 
+                value={formData.img} 
+                onChange={handleChange}
+                />
+            </Col>
+            </Row>
+            <Label>Oil Information:</Label>
+            <Row>
+            <Col md={4}>
+                <Label>Oil Type:</Label>
+                <Input 
+                type='text' 
+                name="Oil Type" 
+                value={formData.oilType} 
+                onChange={handleChange}
+                />
+            </Col>
+            <Col md={4}>
+                <Label>Filter Type:</Label>
+                <Input 
+                type='text' 
+                name="Filter Type" 
+                value={formData.filterType} 
+                onChange={handleChange}
+                />
+            </Col>
+            <Col md={4}>
+                <Label>Capacity:</Label>
+                <Input type='text' value={formData.capacity} onChange={handleChange}/>
+            </Col>
+            </Row>
+            <Button>Submit</Button>
+        </Form>
     )
 }
 
